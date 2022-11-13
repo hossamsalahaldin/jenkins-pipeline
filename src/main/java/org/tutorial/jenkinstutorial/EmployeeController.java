39b25f77b0accd3d22c3e2ff13bab7f6fe4f812e
hniazy <hossam.niazy@cegedim.com>
2022-11-13 13:21:08 +0200

    initial commit

---
+++ src/main/java/org/tutorial/jenkinstutorial/EmployeeController.java
@@ -0,0 +1,25 @@
+package org.tutorial.jenkinstutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @GetMapping("employees")
+    public List<Employee> getEmployees(){
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1,"Hossam Salah",38));
+        employees.add(new Employee(2,"Mostafa Elhefnawy",37));
+        employees.add(new Employee(3,"Reham Huissen",25));
+        employees.add(new Employee(4,"Heba Abdelqader",30));
+        employees.add(new Employee(5,"Omar Mouneer",32));
+        return employees;
+    }
+}
---
+++ src/test/java/org/tutorial/jenkinstutorial/JenkinsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorial.jenkinstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
